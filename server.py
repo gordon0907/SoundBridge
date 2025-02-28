eb0a6189ff61704a3e5358b5b5539f1254108283
gordon0907 <gordonhui0907@gmail.com>
2025-02-28 17:31:26 +0800

    server.py v1.3
    - Use another process to check if default device has changed.

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 import socket
+import time
 from contextlib import suppress
+from multiprocessing import Event, Process
 
 import pyaudio
 
@@ -14,7 +16,7 @@ class SpeakerConfig:
 
     # Derived Configuration
     CHUNK_SIZE = NUM_FRAMES * CHANNELS * 2
-    print(f"Speaker Chunk Size: {CHUNK_SIZE} Bytes")
+    # print(f"Speaker Chunk Size: {CHUNK_SIZE} Bytes")
 
 
 class MicConfig:
@@ -25,7 +27,7 @@ class MicConfig:
 
     # Derived Configuration
     CHUNK_SIZE = NUM_FRAMES * CHANNELS * 2
-    print(f"Microphone Chunk Size: {CHUNK_SIZE} Bytes")
+    # print(f"Microphone Chunk Size: {CHUNK_SIZE} Bytes\n")
 
 
 class Speaker:
@@ -104,17 +106,29 @@ class SoundBridgeServer:
         self.audio_interface = pyaudio.PyAudio()
         self.output_device = self.audio_interface.get_default_output_device_info()
         self.input_device = self.audio_interface.get_default_input_device_info()
+        self.print_current_devices()
 
-        print(f"Playing to: {self.output_device['name']}")
-        print(f"Capturing from: {self.input_device['name']}")
+        # Detect device changes with a multiprocessing event
+        self.changed_event = Event()
+        self.device_monitor = Process(target=device_monitor,
+                                      args=(self.changed_event, self.output_device, self.input_device))
+        self.device_monitor.start()
+
+        # Wait for the process to fully initialize
+        self.changed_event.wait()
+        self.changed_event.clear()
 
         # Instantiate speaker and microphone
         self.speaker: Speaker = Speaker(self)
         self.microphone: Microphone = Microphone(self)
 
-    def __del__(self):
-        self.audio_interface.terminate()
-        self.server_socket.close()
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            traceback.print_exception(exc_type, exc_value, traceback)
+        self.device_monitor.terminate()
 
     def send_data(self, data: bytes):
         """ Sends data to the client. """
@@ -126,6 +140,11 @@ class SoundBridgeServer:
         data, self.client_address = self.server_socket.recvfrom(size)
         return data
 
+    def print_current_devices(self):
+        print(f"Playing to: {self.output_device['name']}")
+        print(f"Capturing from: {self.input_device['name']}")
+        print()
+
     @staticmethod
     def stop_device(instance: Speaker | Microphone):
         if instance.stream is not None:
@@ -136,17 +155,32 @@ class SoundBridgeServer:
             print(f"{instance.__class__.__name__} stopped.")
 
 
+def device_monitor(signal: Event, init_output_device, init_input_device):
+    """ The check must be performed in another process, as PyAudio must be terminated to detect device changes. """
+
+    signal.set()
+    current_output_device = init_output_device
+    current_input_device = init_input_device
+
+    while time.sleep(0.5) or True:
+        print("Debug running", time.time())
+        audio_interface = pyaudio.PyAudio()
+        output_device = audio_interface.get_default_output_device_info()
+        input_device = audio_interface.get_default_input_device_info()
+
+        if output_device != current_output_device or input_device != current_input_device:
+            current_output_device = output_device
+            current_input_device = input_device
+            signal.set()
+
+        audio_interface.terminate()
+
+
 def main():
-    print()
-    server = SoundBridgeServer(server_port=2025)
-    print()
-
-    server.speaker.start()
-    server.microphone.start()
-    input("\n(Press Enter to stop)\n")
-
-    server.speaker.stop()
-    server.microphone.stop()
+    with SoundBridgeServer(server_port=2025) as server:
+        server.speaker.start()
+        server.microphone.start()
+        input("\n(Press Enter to stop)\n")
 
 
 if __name__ == '__main__':
