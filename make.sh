e513d91e0c5eddb4bee2f403edc1907675bdb3e7
gordon0907 <gordonhui0907@gmail.com>
2025-03-03 19:08:47 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
 export PYTHONPATH=/opt/homebrew/opt/portaudio/lib:$PYTHONPATH
 nuitka --clean-cache=all --standalone --onefile --remove-output --output-dir=dist server.py
+mv ./dist/server.bin ./dist/server
